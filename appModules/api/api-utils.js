75c448d96ea037ee08ddb544b25347df21083494
EgorLuk <vupro100sadolbali@gmail.com>
2024-04-19 21:26:08 +0200

    7\

---
+++ appModules/api/api-utils.js
@@ -0,0 +1,18 @@
+async function getData(url){
+    try{
+        const response = await fetch(url)
+        const data = await response.json()
+        return data
+    }
+    catch(error){
+        console.log(error)
+    }
+} 
+// ...функция getData
+
+function getRandomGame(array) {
+    const randomIndex = Math.floor(Math.random() * array.length);
+    return array[randomIndex];
+  }
+module.exports = {getData, getRandomGame}
+
---
+++ appModules/rating/index.js
@@ -0,0 +1,4 @@
+const config = require('./config')
+const makeRatingFile = require('./rating-file')
+const {createRating, updateRating} = require('./calculations')
+module.exports = {config, makeRatingFile, createRating, updateRating}
