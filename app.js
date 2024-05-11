edc855b0234d61b6e1a1b18454fabddbcbc3ece8
shchit20vse <shchitnikov02@mail.ru>
2024-05-11 17:19:34 +0200

    2 неделя

---
+++ app.js
@@ -0,0 +1,27 @@
+const http = require("http");
+const path = require("path");
+const mainRouteController = require("./controllers/main");
+const defaultRouteController = require("./controllers/default");
+const gameRouteController = require("./controllers/game");
+const voteRouteController = require("./controllers/vote")
+
+const PORT = 3005
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    switch (url) {
+        case "/":
+            mainRouteController(res, "/index.html", ".html");
+            break;
+        case "/game":
+            gameRouteController(res);
+            break;
+        case "/vote":
+            voteRouteController(req, res);
+            break;
+        default:
+            defaultRouteController(res, url);
+            break;
+    }   
+});
+
+server.listen(PORT);
---
+++ appModules/api/index.js
@@ -0,0 +1,6 @@
+const { getData, getRandomGame } = require("./api-utils");
+const endpoints = require("./config");
+
+module.exports = {
+    getData, getRandomGame, endpoints
+}
